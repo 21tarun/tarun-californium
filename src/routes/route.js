47be571e5e5777bdb563508dccc125ee1ec52f16
21tarun <tarunuday2000@gmail.com>
2022-12-09 18:43:34 +0530

    commit route module

--- src/routes/route.js
+++ src/routes/route.js
@@ -2,7 +2,11 @@ const express = require('express');
 const router = express.Router();
 const intro = require('./introduction')
 const employee = require('./employee')
-const _ = require('underscore')
+const _ = require('underscore');
+const { movieObjArr } = require('../films/films');
+
+
+
 
 router.get('/test-me', function (req, res) {
     console.log("email from introduction module", intro.myEmail)
@@ -27,9 +31,44 @@ router.get('/', function(req, res){
     
     res.send("@ Home Page")
 })
+const moviesarr=["Rang de basanti", "The shining", "Lord of the rings", "Batman begins"]
 router.get('/movies', function(req, res){
-    const moviesarr=["Rang de basanti", "The shining", "Lord of the rings", "Batman begins"]
     res.send(moviesarr)
 })
+
+router.get('/movies/:indexNumber', function(req, res){
+    if(req.params.indexNumber<moviesarr.length)
+    {
+        res.send(moviesarr[req.params.indexNumber])
+    }
+    else{
+        res.send("Use a valid index")
+    }
+})
+
+
+router.get('/films', function(req, res){
+    res.send(movieObjArr)
+})
+
+router.get('/films/:filmId', function(req, res){
+    let i
+    for(i=0;i<movieObjArr.length;i++)
+    {
+        if(req.params.filmId==movieObjArr[i].id)
+        {
+            res.send(movieObjArr[i].name)
+            return
+        }
+        
+    }
+    if(i==movieObjArr.length )
+    {
+        res.send("No movie exists with this id")
+    }
+
+})
+
+
 
 module.exports = router;
