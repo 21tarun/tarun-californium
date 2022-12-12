144796bf30536009893f17fb29e8027d07c07b1a
21tarun <tarunuday2000@gmail.com>
2022-12-12 16:20:53 +0530

    commit

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,30 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const intro = require('./introduction')
-const employee = require('./employee')
-const _ = require('underscore')
-const mentorModule = require('../abc/xyz/myModule'); 
+
 const req = require('express/lib/request');
 const { route } = require('express/lib/application');
 
-
-router.get("/profile-details", function(req, res){
-    // Write the LOGIC here
-    res.send('dummy response')
-})
-
-router.get('/test-me', function (req, res) {
-    console.log("email from introduction module", intro.myEmail)
-    intro.myFunction('Sabiha')
-    console.log("email from employee module", employee.myEmail)
-
-    const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-    let result = _.first(days, 4)
-    console.log(`Result from underscore function is ${result}`)
-    console.log(`The mentor of the day is ${mentorModule.mentor}`)
-
-    res.send('any dummy text from route handler 1')
-});
 
 
 router.get('/test-me', function(req, res){
@@ -32,46 +11,29 @@ router.get('/test-me', function(req, res){
     res.send("any dummy text from route handler 2")
 })
 
-router.get('/students', function (req, res){
-    let students = ['Sabiha', 'Neha', 'Akash']
-    res.send(students)
+
+router.get("/sol1",function(req,res){
+    let arr1=[1,2,3,5,6,7] //find missing num from this array
+    let arr1Sum= arr1.reduce((a,b)=>a+b,0)
+    let findMissing
+    let a = arr1[0]
+    let d=arr1[1]-arr1[0]
+    let n=arr1.length +1 // here i added 1 :-> we know in our given array one value is missing.
+    let sumOfN= ((2*a + (n-1)*d)*n)/2
+    findMissing =sumOfN -arr1Sum
+    res.send(`missing value is ${findMissing} `)
+
 })
+router.get("/sol2",function(req,res){
+    let arr1=[33, 34, 35, 37, 38] //find missing num from this array
+    let arr1Sum= arr1.reduce((a,b)=>a+b,0)
+    let findMissing
+    let a = arr1[0]
+    let d=arr1[1]-arr1[0]
+    let n=arr1.length +1 // here i added 1 :-> we know in our given array one value is missing.
+    let sumOfN= ((2*a + (n-1)*d)*n)/2
+    findMissing =sumOfN -arr1Sum
+    res.send(`missing value is ${findMissing} `)
 
-// PATH Param example
-router.get('/student-details/:name', function(req, res){
-    /*
-    params is an attribute inside request that contains 
-    dynamic values.
-    This value comes from the request url in the form of an 
-    object where key is the variable defined in code 
-    and value is what is sent in the request
-    */
-
-    let requestParams = req.params
-
-    // JSON strigify function helps to print an entire object
-    // We can use many ways to print an object in Javascript, JSON stringify is one of them
-    console.log("This is the request "+ JSON.stringify(requestParams))
-    let studentName = requestParams.name
-    console.log('Name of the student is ', studentName)
-    
-    res.send('Dummy response')
 })
-
-// PATH Param example
-router.get("/profile/:name", function(req, res){
-    console.log('Printing the request to find out wjere name is stored',req.params)
-    console.log('user name is',req.params.name)
-    //console.log(`User requesting for profile is ${name}`)
-    res.send("dummy details")
-})
-
-// Query Param example
-router.get("/shoes", function(req, res){
-    console.log("The filter options for shoes are -",req.query)
-    //req.query.size
-    //req.query.brand
-    res.send("dummy shoes response")
-})
-
 module.exports = router;
